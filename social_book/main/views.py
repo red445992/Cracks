e89991bdf13717e0c257da649e5b298f0955a17a
aditya <red445992@gmail.com>
2025-02-17 11:36:30 +0545

    Final Push

--- social_book/main/views.py
+++ social_book/main/views.py
@@ -7,6 +7,7 @@ from django.core.exceptions import ObjectDoesNotExist
 
 from .models import profile,post,LikePost,FollowersCount
 from itertools import chain
+import random
 # Index View
 @login_required(login_url='signin')
 def index(request):
@@ -26,11 +27,34 @@ def index(request):
 
     feed_list = list(chain(*feed))
 
+    # user suggestion starts
+    all_users = User.objects.all()
+    user_following_all = []
 
-    posts = post.objects.all()
+    for user in user_following:
+        user_list = User.objects.get(username = user.user)
+        user_following_all.append(user_list)
+    new_suggestions_list = [x for x in list(all_users) if (x not in list(user_following_all))]
+
+    current_user = User.objects.filter(username = request.user.username)
+    final_suggestions_list = [x for x in list(new_suggestions_list) if (x not in list(current_user))]
+    random.shuffle(final_suggestions_list)
+
+    username_profile = []
+    username_profile_list = []
+
+    for users in final_suggestions_list:
+        username_profile.append(users.id)
+    for ids in username_profile:
+        profile_list = profile.objects.filter(id_user = ids)
+        username_profile_list.append(profile_list)
+
+    suggestions_username_profile_list = list(chain(*username_profile_list))
+
     return render(request, 'main/index.html', {
         'user_profile': user_profile,
         'posts': feed_list,
+        'suggestions_username_profile_list':suggestions_username_profile_list[:4],
     })
 
 
